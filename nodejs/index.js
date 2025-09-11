9203b1d286a5a249368e97d11c5e442e6d29fda2
Ajeet Singh Raina <ajeetraina@gmail.com>
2025-09-11 10:16:50 +0530

    Added

---
+++ nodejs/index.js
@@ -0,0 +1,11 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Docker Hardened Node.js!\n');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
