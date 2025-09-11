671d0a04e04ef0d2163f749f640a0ae5b7d79af8
Ajeet Singh Raina <ajeetraina@gmail.com>
2025-09-11 10:18:23 +0530

    Added

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,8 +1,10 @@
 const http = require("http");
+const mimeTypes = require("mime-types");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Docker Hardened Node.js!\n");
+  const contentType = mimeTypes.lookup('html') || 'text/plain';
+  res.writeHead(200, { "Content-Type": contentType });
+  res.end("Hello from Docker Hardened Node.js!\nUsing mime-types package!\n");
 });
 
 server.listen(3000, "0.0.0.0", () => {
